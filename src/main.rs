bc3d3689c2e7f90a29129530e6cfbc0f35a5b569
sashiexists <myself@sashinexists.com>
2022-04-15 12:47:36 +1000

    get_twitter_conversation_from_tweet function created

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use dotenvy::dotenv;
 
 use serde_json;
+use std::any::Any;
 use std::fs::{self};
-use twitter_v2::authorization::BearerToken;
+use twitter_v2::authorization::{BearerToken, Oauth2Token};
+use twitter_v2::data::ReferencedTweet;
+use twitter_v2::data::ReferencedTweetKind::RepliedTo;
 use twitter_v2::query::{TweetField, UserField};
 use twitter_v2::{Tweet, TwitterApi, User};
+
+use async_recursion::async_recursion;
 
 const TWITTER_HANDLE: &str = "yudapearl";
 #[tokio::main]
@@ -31,13 +36,48 @@ async fn main() {
             tweets
         }
     };
+    //let conversation_id = "1513791040263651330";
+    let conversation =
+        get_twitter_conversation_from_tweet(get_tweet_by_id(1514707895992029195).await).await;
+    println!("{:?}", &conversation);
+    fs::write(
+        "conversation.json",
+        serde_json::to_string(&conversation).expect("Failed to parse tweets from API into JSON"),
+    )
+    .expect("Failed to write to conversation.json");
 }
 
-async fn get_tweets_from_user(user: &User) -> Vec<Tweet> {
+//work on this, it will need some recursion
+//update... and it looks like you will need to look into async recursion
+#[async_recursion]
+async fn get_twitter_conversation_from_tweet(tweet: Tweet) -> Vec<Tweet> {
+    let mut output = vec![tweet];
+    match &output[0].referenced_tweets {
+        Some(referenced_tweets) => {
+            if referenced_tweets
+                .iter()
+                .any(|tweet| tweet.kind == RepliedTo)
+            {
+                //you don't want this to be index 0 but rather more precise
+                let replied_to: Tweet = get_tweet_by_id(referenced_tweets[0].id.as_u64()).await;
+                let mut conversation: Vec<Tweet> =
+                    get_twitter_conversation_from_tweet(replied_to).await;
+                output.append(&mut conversation);
+                output
+            } else {
+                output
+            }
+        }
+        None => output,
+    }
+}
+
+//maybe generalised this to get tweets from query
+async fn get_tweets_from_query(query: &str) -> Vec<Tweet> {
     load_api()
         .await
-        .get_user_tweets(user.id)
-        .max_results(20) //this line gets the max results
+        .get_tweets_search_recent(query)
+        .max_results(10)
         .tweet_fields([
             TweetField::Attachments,
             TweetField::ReferencedTweets,
@@ -47,7 +87,26 @@ async fn get_tweets_from_user(user: &User) -> Vec<Tweet> {
         ])
         .send()
         .await
-        .expect("Users tweets net loading")
+        .expect("Failed to get conversation")
+        .into_data()
+        .expect("Failed to open conversation Option<Vec<Tweet>>")
+}
+
+async fn get_tweets_from_user(user: &User) -> Vec<Tweet> {
+    load_api()
+        .await
+        .get_user_tweets(user.id)
+        .max_results(10) //this line gets the max results
+        .tweet_fields([
+            TweetField::Attachments,
+            TweetField::ReferencedTweets,
+            TweetField::ConversationId,
+            TweetField::AuthorId,
+            TweetField::CreatedAt,
+        ])
+        .send()
+        .await
+        .expect("Users tweets not loading")
         .into_data()
         .expect("Failure to open option<Vec<Tweet>>")
 }
