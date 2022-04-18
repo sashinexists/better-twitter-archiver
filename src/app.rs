1e8ab596750d90b5996af434329449cf5b1eb509
sashiexists <myself@sashinexists.com>
2022-04-18 10:06:09 +1000

    add rocket.rs webserver

---
+++ src/app.rs
@@ -0,0 +1,184 @@
+use futures::stream::{self, StreamExt};
+use ron;
+use std::fs::{self};
+use twitter_v2::authorization::BearerToken;
+use twitter_v2::data::ReferencedTweetKind::RepliedTo;
+use twitter_v2::query::{TweetField, UserField};
+use twitter_v2::{Tweet, TwitterApi, User};
+
+use async_recursion::async_recursion;
+
+const TWITTER_HANDLE: &str = "yudapearl";
+#[tokio::main]
+pub async fn main() {
+    //dotenv().ok();
+    let user = load_user().await;
+    let tweets: Vec<Tweet> = load_tweets(&user).await;
+    let conversations = load_conversations(tweets).await;
+}
+
+async fn load_user() -> User {
+    match fs::read_to_string("user.ron") {
+        Ok(user) => {
+            println!("Successfully read user.ron");
+            ron::from_str(&user).expect("Failed to parse file user.ron")
+        }
+        Err(_error) => {
+            println!("Loading User from API");
+            let user = get_user_by_twitter_handle(TWITTER_HANDLE).await;
+            fs::write(
+                "user.ron",
+                ron::to_string(&user).expect("Failed to parse user from API to JSON"),
+            )
+            .expect("Failed to write file user.ron");
+            user
+        }
+    }
+}
+
+async fn load_tweets(user: &User) -> Vec<Tweet> {
+    match fs::read_to_string("tweets.ron") {
+        Ok(tweets) => {
+            println!("Successfully read tweets.ron.");
+            ron::from_str(&tweets).expect("Failed to parse file tweets.ron")
+        }
+        Err(_error) => {
+            println!("Loading tweets from API...");
+            let tweets: Vec<Tweet> = get_tweets_from_user(&user).await;
+            fs::write(
+                "tweets.ron",
+                ron::to_string(&tweets).expect("Failed to parse tweets from API into JSON"),
+            )
+            .expect("Failed to write to tweets.ron");
+            println!("Saved tweets to new file tweets.ron");
+            tweets
+        }
+    }
+}
+
+async fn load_conversations(tweets: Vec<Tweet>) -> Vec<Vec<Tweet>> {
+    match fs::read_to_string("conversations.ron") {
+        Ok(conversations) => {
+            println!("Successully read conversations.ron");
+            ron::from_str(&conversations).expect("Failed to parse file conversations.ron;")
+        }
+        Err(_error) => {
+            println!("Loading conversations from API");
+            let conversations_stream = stream::iter(tweets);
+            let conversations_then =
+                conversations_stream.then(|tweet| get_twitter_conversation_from_tweet(tweet));
+            let conversations = conversations_then.collect::<Vec<_>>().await;
+
+            fs::write(
+                "conversations.ron",
+                ron::to_string(&conversations)
+                    .expect("Failed to parse conversations from API into a JSON file"),
+            )
+            .expect("Failed to write to conversations.ron");
+            conversations
+        }
+    }
+}
+
+#[async_recursion]
+async fn get_twitter_conversation_from_tweet(tweet: Tweet) -> Vec<Tweet> {
+    let mut output = vec![tweet];
+    match &output[0].referenced_tweets {
+        Some(referenced_tweets) => {
+            if referenced_tweets
+                .iter()
+                .any(|tweet| tweet.kind == RepliedTo)
+            {
+                let replied_to_id = referenced_tweets
+                    .iter()
+                    .find(|tweet| tweet.kind == RepliedTo)
+                    .expect("Failed to find replied to tweet")
+                    .id
+                    .as_u64();
+                let replied_to: Tweet = get_tweet_by_id(replied_to_id).await;
+                let mut conversation: Vec<Tweet> =
+                    get_twitter_conversation_from_tweet(replied_to).await;
+                output.append(&mut conversation);
+                output
+            } else {
+                output.reverse();
+                output
+            }
+        }
+        None => output,
+    }
+}
+
+#[allow(dead_code)]
+async fn get_tweets_from_query(query: &str) -> Vec<Tweet> {
+    load_api()
+        .await
+        .get_tweets_search_recent(query)
+        .max_results(10)
+        .tweet_fields([
+            TweetField::Attachments,
+            TweetField::ReferencedTweets,
+            TweetField::ConversationId,
+            TweetField::AuthorId,
+            TweetField::CreatedAt,
+        ])
+        .send()
+        .await
+        .expect("Failed to get conversation")
+        .into_data()
+        .expect("Failed to open conversation Option<Vec<Tweet>>")
+}
+
+async fn get_tweets_from_user(user: &User) -> Vec<Tweet> {
+    load_api()
+        .await
+        .get_user_tweets(user.id)
+        .max_results(10) //this line gets the max results
+        .tweet_fields([
+            TweetField::Attachments,
+            TweetField::ReferencedTweets,
+            TweetField::ConversationId,
+            TweetField::AuthorId,
+            TweetField::CreatedAt,
+        ])
+        .send()
+        .await
+        .expect("Users tweets not loading")
+        .into_data()
+        .expect("Failure to open option<Vec<Tweet>>")
+}
+
+pub async fn get_tweet_by_id(id: u64) -> Tweet {
+    load_api()
+        .await
+        .get_tweet(id)
+        .tweet_fields([
+            TweetField::Attachments,
+            TweetField::ReferencedTweets,
+            TweetField::ConversationId,
+            TweetField::AuthorId,
+            TweetField::CreatedAt,
+        ])
+        .send()
+        .await
+        .expect("this tweet should exist")
+        .into_data()
+        .expect("Failure to open option<Tweet>")
+}
+
+async fn get_user_by_twitter_handle(twitter_handle: &str) -> User {
+    load_api()
+        .await
+        .get_user_by_username(twitter_handle)
+        .user_fields([UserField::Username, UserField::Description])
+        .send()
+        .await
+        .expect("This user should exist")
+        .into_data()
+        .expect("Failure to open Option<User>")
+}
+
+async fn load_api() -> TwitterApi<BearerToken> {
+    let auth = BearerToken::new(std::env::var("TWITTER_DEV_BEARER_TOKEN").unwrap());
+    TwitterApi::new(auth)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,188 +1,82 @@
+#[macro_use]
+extern crate rocket;
 use dotenvy::dotenv;
 
-use futures::executor::block_on;
-use futures::stream::{self, StreamExt};
-use ron;
-use std::any::Any;
-use std::fs::{self};
-use twitter_v2::authorization::BearerToken;
-use twitter_v2::data::ReferencedTweetKind::RepliedTo;
-use twitter_v2::query::{TweetField, UserField};
-use twitter_v2::{Tweet, TwitterApi, User};
+pub mod app;
 
-use async_recursion::async_recursion;
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to a better twitter archiver"
+}
 
-const TWITTER_HANDLE: &str = "yudapearl";
-#[tokio::main]
-async fn main() {
+#[get("/tweet/<id>")]
+async fn tweet_by_id(id: u64) -> String {
+    ron::to_string(&app::get_tweet_by_id(id).await).expect("Failed to serve tweet from id")
+}
+
+#[get("/conversation/<id>")]
+fn conversation_by_id(id: u64) -> String {
+    format!("This routes will get a conversation of id  {}!", id)
+}
+
+#[get("/conversation/<id>/<tweet_id>")]
+fn tweet_in_conversation_by_id(id: u64, tweet_id: u64) -> String {
+    format!(
+        "This routes will get a conversation of id  {id} with the tweet of id {tweet_id} highlighted"
+    )
+}
+
+#[get("/user/<handle>")]
+fn user_by_handle(handle: &str) -> String {
+    format!(
+        "This route will get details on the user with the handle {}",
+        handle
+    )
+}
+
+#[get("/user/<handle>/info")]
+fn user_info_by_handle(handle: &str) -> String {
+    format!(
+        "This route will get info on the user with the handle {}",
+        handle
+    )
+}
+
+#[get("/user/<handle>/tweets")]
+fn tweets_by_user(handle: &str) -> String {
+    format!(
+        "This route will get the tweets on the user with the handle {}",
+        handle
+    )
+}
+
+#[get("/user/<handle>/conversations")]
+fn conversations_by_user(handle: &str) -> String {
+    format!(
+        "This route will get the conversations on the user with the handle {}",
+        handle
+    )
+}
+//in the url the query will look like "/search?query=whatever"
+#[get("/search?<query>")]
+fn search(query: &str) -> String {
+    format!(
+        "This route will run a search of \"{}\" through all the tweets locally stored",
+        query
+    )
+}
+
+#[launch]
+pub fn rocket() -> _ {
     dotenv().ok();
-    let user = load_user().await;
-    let tweets: Vec<Tweet> = load_tweets(&user).await;
-    let conversations = load_conversations(tweets).await;
-}
-
-async fn load_user() -> User {
-    match fs::read_to_string("user.ron") {
-        Ok(user) => {
-            println!("Successfully read user.ron");
-            ron::from_str(&user).expect("Failed to parse file user.ron")
-        }
-        Err(_error) => {
-            println!("Loading User from API");
-            let user = get_user_by_twitter_handle(TWITTER_HANDLE).await;
-            fs::write(
-                "user.ron",
-                ron::to_string(&user).expect("Failed to parse user from API to JSON"),
-            )
-            .expect("Failed to write file user.ron");
-            user
-        }
-    }
-}
-
-async fn load_tweets(user: &User) -> Vec<Tweet> {
-    match fs::read_to_string("tweets.ron") {
-        Ok(tweets) => {
-            println!("Successfully read tweets.ron.");
-            ron::from_str(&tweets).expect("Failed to parse file tweets.ron")
-        }
-        Err(_error) => {
-            println!("Loading tweets from API...");
-            let tweets: Vec<Tweet> = get_tweets_from_user(&user).await;
-            fs::write(
-                "tweets.ron",
-                ron::to_string(&tweets).expect("Failed to parse tweets from API into JSON"),
-            )
-            .expect("Failed to write to tweets.ron");
-            println!("Saved tweets to new file tweets.ron");
-            tweets
-        }
-    }
-}
-
-async fn load_conversations(tweets: Vec<Tweet>) -> Vec<Vec<Tweet>> {
-    match fs::read_to_string("conversations.ron") {
-        Ok(conversations) => {
-            println!("Successully read conversations.ron");
-            ron::from_str(&conversations).expect("Failed to parse file conversations.ron;")
-        }
-        Err(_error) => {
-            println!("Loading conversations from API");
-            let conversations_stream = stream::iter(tweets);
-            let conversations_then =
-                conversations_stream.then(|tweet| get_twitter_conversation_from_tweet(tweet));
-            let conversations = conversations_then.collect::<Vec<_>>().await;
-
-            fs::write(
-                "conversations.ron",
-                ron::to_string(&conversations)
-                    .expect("Failed to parse conversations from API into a JSON file"),
-            )
-            .expect("Failed to write to conversations.ron");
-            conversations
-        }
-    }
-}
-
-#[async_recursion]
-async fn get_twitter_conversation_from_tweet(tweet: Tweet) -> Vec<Tweet> {
-    let mut output = vec![tweet];
-    match &output[0].referenced_tweets {
-        Some(referenced_tweets) => {
-            if referenced_tweets
-                .iter()
-                .any(|tweet| tweet.kind == RepliedTo)
-            {
-                let replied_to_id = referenced_tweets
-                    .iter()
-                    .find(|tweet| tweet.kind == RepliedTo)
-                    .expect("Failed to find replied to tweet")
-                    .id
-                    .as_u64();
-                let replied_to: Tweet = get_tweet_by_id(replied_to_id).await;
-                let mut conversation: Vec<Tweet> =
-                    get_twitter_conversation_from_tweet(replied_to).await;
-                output.append(&mut conversation);
-                output
-            } else {
-                output.reverse();
-                output
-            }
-        }
-        None => output,
-    }
-}
-
-#[allow(dead_code)]
-async fn get_tweets_from_query(query: &str) -> Vec<Tweet> {
-    load_api()
-        .await
-        .get_tweets_search_recent(query)
-        .max_results(10)
-        .tweet_fields([
-            TweetField::Attachments,
-            TweetField::ReferencedTweets,
-            TweetField::ConversationId,
-            TweetField::AuthorId,
-            TweetField::CreatedAt,
-        ])
-        .send()
-        .await
-        .expect("Failed to get conversation")
-        .into_data()
-        .expect("Failed to open conversation Option<Vec<Tweet>>")
-}
-
-async fn get_tweets_from_user(user: &User) -> Vec<Tweet> {
-    load_api()
-        .await
-        .get_user_tweets(user.id)
-        .max_results(10) //this line gets the max results
-        .tweet_fields([
-            TweetField::Attachments,
-            TweetField::ReferencedTweets,
-            TweetField::ConversationId,
-            TweetField::AuthorId,
-            TweetField::CreatedAt,
-        ])
-        .send()
-        .await
-        .expect("Users tweets not loading")
-        .into_data()
-        .expect("Failure to open option<Vec<Tweet>>")
-}
-
-async fn get_tweet_by_id(id: u64) -> Tweet {
-    load_api()
-        .await
-        .get_tweet(id)
-        .tweet_fields([
-            TweetField::Attachments,
-            TweetField::ReferencedTweets,
-            TweetField::ConversationId,
-            TweetField::AuthorId,
-            TweetField::CreatedAt,
-        ])
-        .send()
-        .await
-        .expect("this tweet should exist")
-        .into_data()
-        .expect("Failure to open option<Tweet>")
-}
-
-async fn get_user_by_twitter_handle(twitter_handle: &str) -> User {
-    load_api()
-        .await
-        .get_user_by_username(twitter_handle)
-        .user_fields([UserField::Username, UserField::Description])
-        .send()
-        .await
-        .expect("This user should exist")
-        .into_data()
-        .expect("Failure to open Option<User>")
-}
-
-async fn load_api() -> TwitterApi<BearerToken> {
-    let auth = BearerToken::new(std::env::var("TWITTER_DEV_BEARER_TOKEN").unwrap());
-    TwitterApi::new(auth)
+    rocket::build()
+        .mount("/", routes![search])
+        .mount("/", routes![conversations_by_user])
+        .mount("/", routes![tweets_by_user])
+        .mount("/", routes![user_info_by_handle])
+        .mount("/", routes![user_by_handle])
+        .mount("/", routes![tweet_in_conversation_by_id])
+        .mount("/", routes![conversation_by_id])
+        .mount("/", routes![tweet_by_id])
+        .mount("/", routes![index])
 }
